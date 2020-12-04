0b50de2d53e197f422ddf446aeca76cbc701c73f
3uph0ria <s33rgey@bk.ru>
2020-12-04 23:50:07 +0400

    Task 3.

--- MDK_01_02/LR_5-6/LR_5-6/Program.cs
+++ MDK_01_02/LR_5-6/LR_5-6/Program.cs
@@ -17,7 +17,7 @@ namespace LR_5_6
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new task2());
+            Application.Run(new task3());
         }
     }
 }
---
+++ MDK_01_02/LR_5-6/LR_5-6/forms/task3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LR_5_6.forms
+{
+    public partial class task3 : Form
+    {
+        public task3()
+        {
+            InitializeComponent();
+        }
+
+        private void button_result_Click(object sender, EventArgs e)
+        {
+            double x0 = Convert.ToDouble(input_x0.Text);
+            double xk = Convert.ToDouble(input_xk.Text);
+            double dx = Convert.ToDouble(input_dx.Text);
+            double a = Convert.ToDouble(input_a.Text);
+            result.Text = "Работу выполнил ст. Саломатин С.В." + Environment.NewLine;
+
+            double x = x0;
+
+            while (x <= (xk + dx / 2))
+            {
+                double y = 9 * (x + 15 * Math.Sqrt(Math.Pow(x, 3) + Math.Pow(a, 3)));
+                result.Text += "x=" + Convert.ToString(x) +
+                "; y=" + Convert.ToString(y) +
+                Environment.NewLine;
+                x = x + dx;
+
+                // 9x4 + sin(57.2 + x),
+            }
+        }
+    }
+}
